5ed06f58143a0144c295be20af46a4f6e5f38373
MiriMakesMeow <74431274+MiriMakesMeow@users.noreply.github.com>
2024-06-11 11:13:13 +0200

    Aktualisieren von import_and_processing.py
    No findings Liste für autoencoder

--- import_and_processing.py
+++ import_and_processing.py
@@ -3,96 +3,79 @@ import os
 import pandas as pd
 import numpy as np
 
-def load_and_preprocess(image_paths, labels_path, num_images, use_half = True):
-    
-    labels_df = pd.read_csv(labels_path)    # import labels
+def load_and_preprocess(image_paths, labels_path, num_images, use_half=True):
+    labels_df = pd.read_csv(labels_path)  # Import labels
 
-    # import as np
     if use_half:
         num_images = len(image_paths) // 2
         image_paths = image_paths[:num_images]
     image_width, image_height = 256, 256
-    num_channels = 1 # gray
-    images = np.zeros((num_images, image_height, image_width, num_channels), dtype=np.uint8)
-    labels = np.zeros((num_images), dtype=int)
-    valid_index = 0
+    num_channels = 1  # gray
+
+    no_finding_images = []
+    finding_images = []
 
     for path in image_paths:
         image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        # check if image was loaded correctly
         if image is None:
             continue
-        
+
         resized_image = cv2.resize(image, (256, 256))
-        # extract file name
         file_name = os.path.basename(path)
 
-        # extract label from dataframe and add to array
         label_row = labels_df[labels_df['Image Index'] == file_name]
         if label_row.empty:
             continue
         label = label_row["Finding Labels"].values[0]
         if "No Finding" in label:
             label = 0
+            no_finding_images.append(resized_image.reshape(256, 256, num_channels))
         else:
             label = 1
+            finding_images.append(resized_image.reshape(256, 256, num_channels))
 
-        labels[valid_index] = label 
+    no_finding_images = np.array(no_finding_images).astype("float16") / 255.0
+    finding_images = np.array(finding_images).astype("float16") / 255.0
 
-        # add image to array, incl reshaping
-        images[valid_index] = resized_image.reshape(256, 256, num_channels)
+    np.save("no_finding_images.npy", no_finding_images)
+    np.save("finding_images.npy", finding_images)
 
-        valid_index += 1
-        if valid_index % 1000 == 0:
-            print(f'Loaded Image No. {valid_index}')
+    print(f'Loaded {len(no_finding_images)} no finding images')
+    print(f'Loaded {len(finding_images)} finding images')
 
-    # cut arrays to correct num
-    images = images[:valid_index]
-    labels = labels[:valid_index]
-
-    # normalize and save
-    images = images.astype("float16")/ 255.0
-
-    np.save("images.npy", images)
-    np.save("labels.npy", labels)
-
-    print(f'Loaded {valid_index} Images')
-
-    return images, labels, valid_index
+    return no_finding_images, finding_images
 
 def load_saved_images():
-    images = np.load("images.npy")
-    labels = np.load("labels.npy")
-    valid_index = len(images)
+    no_finding_images = np.load("no_finding_images.npy")
+    finding_images = np.load("finding_images.npy")
 
-    print(f'Loaded {valid_index} images')
-    return images, labels, valid_index
-
+    print(f'Loaded {len(no_finding_images)} no finding images')
+    print(f'Loaded {len(finding_images)} finding images')
+    return no_finding_images, finding_images
 
 def load_images_and_labels(image_paths, labels_path):
-    labels_df = pd.read_csv(labels_path)    # import labels
+    labels_df = pd.read_csv(labels_path)  # Import labels
 
-    # import as list, no copy
     labeled_images = []
 
     for file_path in image_paths:
-        image = cv2.imread(file_path)
-        if image is None: # check if image was loaded correctly
-            continue # skip
+        image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        if image is None:  # Check if image was loaded correctly
+            continue  # Skip
 
-        file_name = os.path.basename(file_path) # extract file name
+        file_name = os.path.basename(file_path)  # Extract file name
 
-        label_row = labels_df.loc[labels_df['Image Index'] == file_name] # match labels with filename
+        label_row = labels_df.loc[labels_df['Image Index'] == file_name]  # Match labels with filename
 
-        if label_row.empty: 
-            continue # skip if label row didn't have label
-        label = label_row['Finding Labels'].values[0] # extract labels
+        if label_row.empty:
+            continue  # Skip if label row didn't have label
+        label = label_row['Finding Labels'].values[0]  # Extract labels
 
-        # check if label is finding
+        # Check if label is finding
         if "No Finding" in label:
-            label = 0   # "No Finding"
+            label = 0  # "No Finding"
         else:
-            label = 1   # "Finding"
+            label = 1  # "Finding"
         labeled_images.append((image, label))
 
     print('Loaded Images')
