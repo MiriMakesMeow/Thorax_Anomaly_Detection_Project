41f9f7006097cf60e1fd9de4b56d6eb70ad72520
MiriMakesMeow <74431274+MiriMakesMeow@users.noreply.github.com>
2024-06-19 13:32:33 +0200

    Update main.py
    added slicing in train, val and test data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import glob
 import numpy as np
 from import_and_processing import load_images_and_labels, load_and_preprocess, load_saved_images
-from data_slicing import shuffle_and_split
-from autoencoder import autoencoder
+from sklearn.model_selection import train_test_split
+from autoencoder import build_autoencoder, plot_training_history
 from keras import callbacks
 
 # Daten importieren und vorverarbeiten
@@ -11,15 +11,30 @@ num_images = len(image_paths)
 labels_path = "./Thorax/labels.csv"
 
 # Bilder und Labels laden und vorverarbeiten
-no_findings, findings = load_and_preprocess(image_paths, labels_path, num_images, use_half=True)
+#no_findings, findings = load_and_preprocess(image_paths, labels_path, num_images, use_half=True)
 # oder, wenn bereits gespeichert:
-# no_findings, findings = load_saved_images()
+no_findings, findings = load_saved_images()
 
 # Daten aufteilen
-# TODO: auf no findings anpassen X_train, y_train, X_val, y_val, X_test, y_test = shuffle_and_split(images, labels, valid_index)
+indices_nf = np.arange(len(no_findings))
+
+# shuffling indices 
+np.random.shuffle(indices_nf)
+
+# slice data 90% training, 10% valid
+train_split = int(0.9 * len(no_findings))
+
+train_indices = indices_nf[:train_split]
+val_indices = indices_nf[train_split:]
+
+# extract data
+X_train = no_findings[train_indices]
+X_val = no_findings[val_indices]
+X_test = findings
 
 # Autoencoder initialisieren
-model = autoencoder
+input_shape = (256, 256, 1)
+model = build_autoencoder(input_shape)
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -28,7 +43,8 @@ epochs = 100
 batch_size = 32
 
 early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-model.fit(X_train, X_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, X_val), callbacks=[early_stopping])
+history = model.fit(X_train, X_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, X_val), callbacks=[early_stopping])
+plot_training_history(history)
 
 # Anomalieerkennung auf Testdaten
 def compute_reconstruction_error(original, reconstruction):
